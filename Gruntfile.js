a0231abe26209628bbb50a4e0f8eab1d1b44df8f
Marc-Antoine Lemieux <marc@marcantoinelemieux.com>
2015-05-29 08:02:37 -0400

    made task in grunt not single run, but karma conf yes

--- Gruntfile.js
+++ Gruntfile.js
@@ -74,7 +74,8 @@ module.exports = function(grunt) {
 
         karma: {
             unit: {
-                configFile: 'karma.conf.js'
+                configFile: 'karma.conf.js',
+                singleRun: false
             }
         },
 
